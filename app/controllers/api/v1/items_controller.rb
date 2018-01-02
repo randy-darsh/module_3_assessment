498d62becff404fe081ac2de1dfef9d601997748
randy-darsh <rspringer47@gmail.com>
2018-01-02 10:34:53 -0700

    working on 4th test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,9 +8,23 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.find(params[:id])
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render json: Item.find(params[:id])
+    else
+      redirect_to 'rake route'
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.delete
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
   end
 
 end
--- spec/requests/api/v1/items/request_spec.rb
+++ spec/requests/api/v1/items/request_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 describe 'User visits /api/v1/items' do
-  before :each do
-    create_list(:item, 5)
-  end
 
   it 'they recieve a 200 JSON response containing all items' do
+    create_list(:item, 5)
     get '/api/v1/items'
 
     expect(response.status).to eq(200)
@@ -17,6 +15,7 @@ describe 'User visits /api/v1/items' do
   end
 
   it 'they visit item show JSON page' do
+    create_list(:item, 5)
     get '/api/v1/items/1'
 
     expect(response.status).to eq(200)
@@ -29,11 +28,24 @@ describe 'User visits /api/v1/items' do
     # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
   end
 
-  it '' do
+  it 'they send a delete request and the object is deleted' do
+    create_list(:item, 5)
     delete '/api/v1/items/1'
 
     expect(response.status).to eq(204)
     # When I send a DELETE request to `/api/v1/items/1`
     # I receive a 204 JSON response if the record is successfully deleted
+  end
+
+  it 'they can create a JSON object with a post request' do
+    item = create(:item)
+    post '/api/v1/items', params: { item: item }
+
+    expect(response.status).to eq(201)
+
+    parsed_response = JSON.parse(response.body)
+    # When I send a POST request to `/api/v1/items` with a name, description, and image_url
+    # I receive a 201 JSON  response if the record is successfully created
+    # And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
   end
 end
