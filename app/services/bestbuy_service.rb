82e4caf1524d7deedd224390a239e6ee2a76759f
randy-darsh <rspringer47@gmail.com>
2018-01-02 11:46:44 -0700

    got api to return correct information

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -4,8 +4,10 @@ class BestbuyService
   end
 
   def stores
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(postalCode=#{filter[:postalCode]})?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV["BESTBUY_KEY"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{filter[:location]},25))?format=json&show=storeType,longName,city,distance,phone&pageSize=17&apiKey=#{ENV["BESTBUY_KEY"]}")
+    # response = Faraday.get("https://api.bestbuy.com/v1/stores(postalCode=#{filter[:postalCode]})?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV["BESTBUY_KEY"]}")
     @stores = JSON.parse(response.body, symbolize_names: true)[:stores].map do |raw_store|
+      binding.pry
       Store.new(raw_store)
     end
   end
